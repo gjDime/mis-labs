0359dac4936fc57db57d4279a0bd2792bbd78eee
Dime Gjorgjiev <dime@codechem.com>
2025-12-07 21:49:06 +0100

    Add firebase connection

--- the_meal/lib/main.dart
+++ the_meal/lib/main.dart
@@ -1,10 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:the_meal/firebase_options.dart';
 import 'package:the_meal/screens/categories.dart';
 import 'package:the_meal/screens/favourites.dart';
 import 'package:the_meal/screens/meal_details.dart';
 import 'package:the_meal/screens/meals.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  String? token = await FirebaseMessaging.instance.getToken();
+  print('FCM Token: $token');
+
   runApp(const MyApp());
 }
 
@@ -14,7 +23,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'The Meal app',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink),
       ),
